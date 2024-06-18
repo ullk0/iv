7475dd8ca46865952d22b8587aabe640e7963318
Ulyana Danilevskaya <usdanilevskaya@gmail.com>
2024-06-18 15:17:57 +0300

    Form touch ups

--- js/mrc.js
+++ js/mrc.js
@@ -1,3 +1,5 @@
+// DATE CHOICE LIMITATION
+
 function setDatepickerMinDate(date, id) {
     const element = document.getElementById(id)
     element.min = date
@@ -8,13 +10,11 @@ function setDatepickerMaxDate(date, id) {
     element.max = date
 }
 
-
 Date.prototype.addDays = function (days) {
     var date = new Date(this.valueOf());
     date.setDate(date.getDate() + days);
     return date;
 }
-
 
 function getMinDate() {
     const now = (new Date()).addDays(1)
@@ -28,3 +28,14 @@ function getMaxDate() {
 
 setDatepickerMinDate(getMinDate(), "datepick")
 setDatepickerMaxDate(getMaxDate(), "datepick")
+
+// SUBMIT BUTTON
+
+var form = document.getElementById("register");
+function handleForm(event) { event.preventDefault(); }
+form.addEventListener('submit', handleForm);
+
+function submitForm() {
+    document.getElementById("register").style.display = "none";
+    document.getElementById("reg--onsubmit").style.display = "block"
+}
